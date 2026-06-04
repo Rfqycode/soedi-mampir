dbece0160a5e29f5ad1ff3be559d7b721a16668b
Rfqycode <muh.rifqy.asdar@gmail.com>
2026-06-05 00:31:06 +0800

    fix: ganti database ke sqlite untuk rilis darurat

--- backend/barber_backend/settings.py
+++ backend/barber_backend/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'barber_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db_barber', # Pastikan kamu sudah bikin database kosong dengan nama ini di phpMyAdmin
-        'USER': 'root',      # Default user XAMPP biasanya 'root'
-        'PASSWORD': '',      # Default password XAMPP biasanya kosong
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
