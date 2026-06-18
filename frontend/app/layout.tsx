4c9760c435f9e22f8b26effadd9d34cd4434c82d
Rfqycode <muh.rifqy.asdar@gmail.com>
2026-06-18 21:37:50 +0800

    update logo tab website barbershop

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -9,6 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Soedi Mampir Barbershop",
   description: "...",
+  
 };
 
 export default function RootLayout({
